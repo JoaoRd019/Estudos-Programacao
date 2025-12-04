e0ad623f6ea02672b552b96550fec31975af35eb
JoaoRd019 <joao_kassis@hotmail.com>
2025-12-04 14:55:51 -0300

    fix

--- gorjeta/main.py
+++ gorjeta/main.py
@@ -1,26 +1,23 @@
-def mensagem():
-    print(f"\nValor da conta: R$ {valor_conta:.2f}")
-    print(f"Valor da gorjeta: R$ {conta:.2f}")
 valor_conta = float(input("Digite o valor da conta para calcular a gorjeta: "))
-
-print("Escolha a porcentagem da gorjeta")
+print("\nEscolha a porcentagem da gorjeta")
 print("1. Gorjeta 10%")
 print("2. Gorjeta 15%")
 print("3. Gorjeta 20%")
-opcao = input("- Digite aqui:  ")
+opcao = input("- Digite aqui: ")
+
+def mensagem():
+    print(f"\nValor da conta: R$ {valor_conta:.2f}")
+    print(f"Valor da gorjeta: R$ {conta:.2f}")
+    print(f"Total a pagar: R$ {valor_conta + conta:.2f}")
 
 if opcao == "1":
     conta = valor_conta * 0.1
     mensagem()
-
 elif opcao == "2":
     conta = valor_conta * 0.15
     mensagem()
-
 elif opcao == "3":
     conta = valor_conta * 0.2
     mensagem()
-
 else:
-    print("Digite uma opção valida ")
-    
+    print("Digite uma opção válida")
